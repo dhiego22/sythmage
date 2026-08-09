a4332c8d4f0a3b1d22a1e1a6d59c01a18f065b2d
dhiego22 <42843427+dhiego22@users.noreply.github.com>
2026-08-09 13:15:04 -0700

    Update train.py
    removed config part

--- train.py
+++ train.py
@@ -1,14 +1,3 @@
-dir_base = ""
-dir_target = ""
-
-epochs = 10
-batch_size = 1
-lr = 2e-4
-lambda_l1 = 100
-patch_size = (64,64,64)
-patches_per_volume = 32
-checkpoints_dir = 'checkpoints'
-
 #Setup
 random.seed(42)
 np.random.seed(42)
