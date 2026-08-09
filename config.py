20105b101ae27a7dd1b18fd891f43a37bac29496
dhiego22 <42843427+dhiego22@users.noreply.github.com>
2026-08-09 13:16:11 -0700

    Update config.py
    added dataclass

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 dir_base = ""
 dir_target = ""
-
-
-epochs = 10
-batch_size = 1
-lr = 2e-4
-lambda_l1 = 100
-patch_size = (64,64,64)
-patches_per_volume = 32
 checkpoints_dir = 'checkpoints'
+
+from dataclasses import dataclass
+
+@dataclass
+class TrainConfig:
+    epochs: int = 100
+    batch_size: int = 1
+    lr: float = 2e-4
+    lambda_l1: int = 100
+    patch_size: tuple = (64,64,64)
+    patches_per_volume: int = 32
