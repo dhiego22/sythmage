0ad0caac7715f2e7e78ff8a207f8b83be73b1337
dhiego22 <42843427+dhiego22@users.noreply.github.com>
2026-08-12 12:28:03 -0700

    Update datasets.py
    updated datasets

--- datasets.py
+++ datasets.py
@@ -6,59 +6,59 @@ import nibabel as nib
 import numpy as np
 import torch
 from torch.utils.data import Dataset
-from .utils import minmax_norm
+from utils import minmax_norm
 
 class PairedMRI3DPatches(Dataset):
-    def __init__(self, dir_3t:str, dir_7t:str, 
+    def __init__(self, dir_base:str, dir_target:str, 
                  patch_size: Tuple[int,int,int]=(64,64,64),
                  patches_per_volume: int=16):
 
-        files_3t = sorted(glob.glob(os.path.join(dir_3t, '*.nii.gz'))) # or *.nii
-        files_7t = sorted(glob.glob(os.path.join(dir_7t, '*.nii.gz'))) # or *.nii
+        files_base = sorted(glob.glob(os.path.join(dir_base, '*.nii.gz'))) # or *.nii
+        files_target = sorted(glob.glob(os.path.join(dir_target, '*.nii.gz'))) # or *.nii
         self.patch_size = patch_size
         self.patches_per_volume = patches_per_volume
 
         def pairing_key(p:str) -> str:
             return os.path.splitext(os.path.basename(p))[0]
 
-        map_3t, map_7t = {},  {}
-        for p in files_3t:
-            map_3t[pairing_key(p)] = p
-        for p in files_7t:
-            map_7t[pairing_key(p)] = p
-        common = sorted(set(map_3t) & set(map_7t))
+        map_base, map_target = {},  {}
+        for p in files_base:
+            map_base[pairing_key(p)] = p
+        for p in files_target:
+            map_target[pairing_key(p)] = p
+        common = sorted(set(map_base) & set(map_target))
         if len(common) == 0:
-            raise FileNotFoundError(f"No paired files; check directories. 3T={dir_3t} 7T={dir_7t}")
-        self.pairs: List[Tuple[str,str]] = [(map_3t[k], map_7t[k]) for k in common]#Build a list of paired file paths (tuples (3T_path, 7T_path)), aligned by the same pairing key.
+            raise FileNotFoundError(f"No paired files; check directories. base={dir_base} target={dir_target}")
+        self.pairs: List[Tuple[str,str]] = [(map_base[k], map_target[k]) for k in common]#Build a list of paired file paths (tuples (3T_path, 7T_path)), aligned by the same pairing key.
         
-        self.cache_3t: List[np.ndarray] = [None] * len(self.pairs) # caches for loaded volumes (numpy arrays).
-        self.cache_7t: List[np.ndarray] = [None] * len(self.pairs) #volumes are only loaded when first used, then kept in memory for re-use during the epoch (reducing disk I/O)
+        self.cache_base: List[np.ndarray] = [None] * len(self.pairs) # caches for loaded volumes (numpy arrays).
+        self.cache_target: List[np.ndarray] = [None] * len(self.pairs) #volumes are only loaded when first used, then kept in memory for re-use during the epoch (reducing disk I/O)
 
     def __len__(self):
             return len(self.pairs) * self.patches_per_volume
 
     def _get_volumes(self, idx_pair:int):
-            v3, v7 = self.cache_3t[idx_pair], self.cache_7t[idx_pair]
-            if v3 is None:
-                p3, p7 = self.pairs[idx_pair]
-                v3 = nib.load(p3).get_fdata().astype(np.float32)
-                v7 = nib.load(p7).get_fdata().astype(np.float32)
-                v3 = minmax_norm(v3)
-                v7 = minmax_norm(v7) 
-                self.cache_3t[idx_pair] = v3
-                self.cache_7t[idx_pair] = v7
-            return v3, v7
+            v=vb, vt = self.cache_base[idx_pair], self.cache_target[idx_pair]
+            if vb is None:
+                pb, pt = self.pairs[idx_pair]
+                vb = nib.load(pb).get_fdata().astype(np.float32)
+                vt = nib.load(pt).get_fdata().astype(np.float32)
+                vb = minmax_norm(vb)
+                vt = minmax_norm(vt) 
+                self.cache_base[idx_pair] = vb
+                self.cache_target[idx_pair] = vt
+            return vb, vt
 
     def __getitem__(self, idx:int):
             pair_idx = idx // self.patches_per_volume# //  floor division assignment operator. Choosing which pair to use
-            v3, v7 = self._get_volumes(pair_idx)
-            H, W, D = v3.shape
+            vb, vt = self._get_volumes(pair_idx)
+            H, W, D = vb.shape
             ph, pw, pd = self.patch_size
             sh = random.randint(0, max(0, H - ph)) if H >= ph else 0 #  patch extraction
             sw = random.randint(0, max(0, W - pw)) if W >= pw else 0
             sd = random.randint(0, max(0, D - pd)) if D >= pd else 0
-            patch3 = v3[sh:sh+ph, sw:sw+pw, sd:sd+pd]
-            patch7 = v7[sh:sh+ph, sw:sw+pw, sd:sd+pd]
+            patchb = vb[sh:sh+ph, sw:sw+pw, sd:sd+pd]
+            patcht = vt[sh:sh+ph, sw:sw+pw, sd:sd+pd]
             def pad_to_shape(x, target):
                 pad_h = max(0, target[0] - x.shape[0])
                 pad_w = max(0, target[1] - x.shape[1])
@@ -66,8 +66,8 @@ class PairedMRI3DPatches(Dataset):
                 if pad_h or pad_w or pad_d:
                     x = np.pad(x, ((0, pad_h), (0, pad_w), (0, pad_d)), mode='reflect')
                 return x
-            patch3 = pad_to_shape(patch3, self.patch_size)#Ensure both patches are exactly (ph, pw, pd)
-            patch7 = pad_to_shape(patch7, self.patch_size)
-            t3 = torch.from_numpy(patch3).permute(2,0,1).unsqueeze(0).float()# Convert numpy arrays → PyTorch tensors (float32)
-            t7 = torch.from_numpy(patch7).permute(2,0,1).unsqueeze(0).float()#final shapes: (1, pd, ph, pw) applying .unsqueeze(0)
-            return t3, t7
+            patchb = pad_to_shape(patchb, self.patch_size)#Ensure both patches are exactly (ph, pw, pd)
+            patcht = pad_to_shape(patcht, self.patch_size)
+            tb = torch.from_numpy(patchb).permute(2,0,1).unsqueeze(0).float()# Convert numpy arrays → PyTorch tensors (float32)
+            tt = torch.from_numpy(patcht).permute(2,0,1).unsqueeze(0).float()#final shapes: (1, pd, ph, pw) applying .unsqueeze(0)
+            return tb, tt
