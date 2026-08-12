2c7322b21910ac5516dd61c578f617edbe7554fa
dhiego22 <42843427+dhiego22@users.noreply.github.com>
2026-08-12 12:51:27 -0700

    Update main.py
    updated main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 from train import train
-from reconstruct import reconstruct
+from inference import reconstruct
 
 def main()-> None:
     parser = argparse.ArgumentParser(prog='sythmage', description='Synthetic 3D medical images translator')
@@ -13,7 +13,7 @@ def main()-> None:
     parser_train.add_argument('--epochs', type=int, default=10)
     parser_train.add_argument('--batch-size', type=int, default=1)
     parser_train.add_argument('--lr', type=float, default=2e-4)
-    parser_train.add_argument('--lambda-l1', type=float,default=100)
+    parser_train.add_argument('--lambda-r1', type=float,default=10)
     parser_train.add_argument('--patch-size', type=int, nargs=3, default=[64,64,64])
     parser_train.add_argument('--patches-per-volume', type=int, default=16)
     parser_train.add_argument('--metrics-dir', default='metrics')
@@ -33,16 +33,15 @@ def main()-> None:
     args = parser.parse_args()
     if args.cmd == 'train':
         train(
-            model = args.model,
             dir_base = args.dir_base,
             dir_target = args.dir_target,
             epochs = args.epochs,
             batch_size = args.batch_size,
             lr = args.lr,
-            lambda_l1 = args.lambda_l1,
-            patch_size = tuple(args.patch_size),
-            patches_per_volume = args.patches_per_volume,
-            checkpoints_dir = args.checkpoints_dir,
+            lambda_r1 = args.lambda_r1,
+            patch_size = (64,64,64),
+            patches_per_volume = 32,
+            save_dir = args.checkpoints_dir,
             
             
         )
